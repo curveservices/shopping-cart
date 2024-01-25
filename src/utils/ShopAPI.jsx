6992ba5d5635c131d07fda11746857d595ca73ad
Paul <p.rossiter83@gmail.noreply.github.com>
2024-01-25 08:53:46 +0000

    Clean up added

--- src/utils/ShopAPI.jsx
+++ src/utils/ShopAPI.jsx
@@ -2,26 +2,38 @@ import { useEffect, useState } from "react";
 
 export default function useAPI(url) {
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [loaded, setLoaded] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+
     setLoading(true);
-    fetch(url)
+    setLoaded(false);
+    setError(null);
+
+    fetch(url, { signal })
       .then((response) => response.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-        console.log(data);
+      .then((responseData) => {
+        if (!signal.aborted) {
+          setData(responseData);
+          setLoaded(true);
+          setLoading(false);
+        }
       })
-      .catch((error) => {
-        setError(error.message);
-        setData(null);
-      })
-      .finally(() => {
-        setLoading(false);
+      .catch((fetchError) => {
+        if (!signal.aborted) {
+          setError(fetchError.message);
+          setData(null);
+          setLoading(false);
+        }
       });
+
+    return () => abortController.abort(); // Cleanup on component unmount
+
   }, [url]);
 
-  return { data, loading, error };
+  return { data, loading, loaded, error };
 }
