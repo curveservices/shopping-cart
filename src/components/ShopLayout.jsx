6486c6ad668e5e7c1f5658229a78c6fd69f6aa9c
Paul <p.rossiter83@gmail.noreply.github.com>
2024-02-01 09:59:20 +0000

    [Feature] navLink added

--- src/components/ShopLayout.jsx
+++ src/components/ShopLayout.jsx
@@ -5,6 +5,7 @@ const ShopLayout = () => {
   return (
     <div className={styles.layout}>
       <nav className={styles.sidebar}>
+        <NavLink to="/shop">All Products</NavLink>
         <NavLink to="womenswear">Women's wear</NavLink>
         <NavLink to="menswear">Men's wear</NavLink>
         <NavLink to="jewelery">Jewelery</NavLink>
